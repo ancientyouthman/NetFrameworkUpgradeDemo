38d357350803138a5cd9027f359dcf3a89b5124d
Joel Godden <Joel.Godden@tpr.gov.uk>
2024-02-19 20:50:26 +0000

    clean slate....

---
+++ NetFrameworkApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using NetFrameworkApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFrameworkApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
